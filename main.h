c7c5e46bb39f0209f8e03d12648292c47d0cc621
root <amr.magdy.hassan95@gmail.com>
2023-03-27 14:57:22 -0700

    Feature: Added the main structure of the code

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+int _printf(const char *format, ...);
+void handle_backslash(int *sum, char follow_char);
+int _putchar(char c);
+void handle_string(int *sum, char *s);
+void handle_int(int *sum, int d);
+void handle_percision(char f, int *sum, va_list ap, int *i);
+#endif
---
+++ printf.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - a simple clone of c printf
+ * @format: a format specifying what to print to the screen
+ * Return: the number of characters in the string
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, sum = 0, d;
+	char c, *s;
+	va_list ap;
+
+	va_start(ap, format);
+	if (format == NULL)
+		return (-1);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '\\')
+		{
+			handle_backslash(&sum, format[i + 1]);
+		}
+		else if (format[i] == '&')
+		{
+			handle_percision(format[i + 1], &sum, ap, &i);
+		}
+		else
+		{
+			_putchar(format[i]);
+			sum++;
+		}
+		i++;
+
+	}
+	va_end(ap);
+	return (sum);
+}
