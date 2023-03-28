1d51cefa28d1f01eff17faa0274d154d4bddf36b
ahmed_mustafa <ahmed_mustafa@mail.com>
2023-03-27 17:05:02 -0700

    adding condition into the loop of printf by return -1

--- printf.c
+++ printf.c
@@ -18,8 +18,8 @@ int _printf(const char *format, ...)
 		return (-1);
 	while (format[i] != '\0')
 	{
-		if (format[i] == '\0')
-			return (sum);
+		if (format[i + 1] == '\0')
+			return (-1);
 
 		if (format[i] == '\\')
 		{
